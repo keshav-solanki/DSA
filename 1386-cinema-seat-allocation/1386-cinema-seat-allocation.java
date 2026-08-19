45927f85af5928569737e643471de5681dfb2ab6
Keshav Solanki <113750127+keshav-solanki@users.noreply.github.com>
2026-08-19 22:17:33 +0530

    Time: 19 ms (71.05%), Space: 50.9 MB (87.89%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
@@ -1,61 +1,38 @@
+import java.util.*;
+
 class Solution {
     public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
-    //     boolean[][] visited = new boolean[n+1][10];
 
-    //     for(int[] seats : reservedSeats ){
-    //         int row = seats[0];
-    //         int seat = seats[1];
-    //         visited[row][seat] = true;
-    //     }
-    //     int maxGroup =0;
-    //     for(int i = 1; i<=n; i++){
-    //         boolean left = isFree(visited,2,5,i);
-    //         boolean mid = isFree(visited,4,7,i);
-    //         boolean right = isFree(visited,6,9,i);
+        Map<Integer, Integer> map = new HashMap<>();
 
+        for (int[] seat : reservedSeats) {
+            int row = seat[0];
+            int col = seat[1];
 
-    //         if(left && right) maxGroup += 2;
-    //         else if(left || mid || right ) maxGroup += 1;
-
-    //     } 
-    //     return maxGroup;  
-    // }
-
-    // public boolean isFree(boolean [][] visited,int s, int e,int i){
-    //     for(int j=s ; j<=e; j++){
-    //         if(visited[i][j] ) return false;
-    //     }
-    //     return true;
-
-        HashMap<Integer, boolean[]> map = new HashMap<>();
-
-        for(int[] seats : reservedSeats ){
-            int row = seats[0];
-            int seat = seats[1];
-            boolean[] arr = map.getOrDefault(row,new boolean[11]);
-            arr[seat] = true;
-            map.put(row,arr);
+            map.put(row, map.getOrDefault(row, 0) | (1 << (col - 1)));
         }
-        int maxGroup = 0;
-        maxGroup += (n-map.size())*2;
 
-        for (Map.Entry<Integer,boolean[] > entry : map.entrySet()) {
-            int row = entry.getKey();
-            boolean[] seats = entry.getValue();
-            boolean left = isFree(seats,2,5);
-            boolean mid = isFree(seats,4,7);
-            boolean right = isFree(seats,6,9);
+        // Rows with no reservations can always accommodate 2 groups
+        long ans = (long) (n - map.size()) * 2;
 
+        for (int mask : map.values()) {
 
-            if(left && right) maxGroup += 2;
-            else if(left || mid || right ) maxGroup += 1;
+            // Seats 2,3,4,5
+            boolean left = (mask & 0b000011110) == 0;
+
+            // Seats 4,5,6,7
+            boolean middle = (mask & 0b001111000) == 0;
+
+            // Seats 6,7,8,9
+            boolean right = (mask & 0b111100000) == 0;
+
+            if (left && right) {
+                ans += 2;
+            } else if (left || middle || right) {
+                ans += 1;
+            }
         }
-        return maxGroup;
-    }
-    public boolean isFree(boolean [] visited,int s, int e){
-        for(int j=s ; j<=e; j++){
-            if(visited[j] ) return false;
-        }
-        return true;
+
+        return (int) ans;
     }
 }
