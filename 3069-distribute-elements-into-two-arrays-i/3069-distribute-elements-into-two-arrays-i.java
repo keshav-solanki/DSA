74e702dd93b14c225170668fa44c489a9053dd31
Keshav Solanki <113750127+keshav-solanki@users.noreply.github.com>
2026-08-20 23:26:44 +0530

    Time: 2 ms (57.45%), Space: 47 MB (12.74%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
@@ -1,28 +1,38 @@
+import java.util.*;
+
 class Solution {
     public int[] resultArray(int[] nums) {
-       List<Integer> arr1 = new ArrayList<>();
-       List<Integer> arr2 = new ArrayList<>();
+        List<Integer> arr1 = new ArrayList<>();
+        List<Integer> arr2 = new ArrayList<>();
 
-       arr1.add(nums[0]);
-       arr2.add(nums[1]);
+        // First two operations
+        arr1.add(nums[0]);
+        arr2.add(nums[1]);
 
-        for(int i =2; i<nums.length; i++){
-            if(arr1.get(arr1.size()-1) > arr2.get(arr2.size()-1)){
+        // Remaining elements
+        for (int i = 2; i < nums.length; i++) {
+            int last1 = arr1.get(arr1.size() - 1);
+            int last2 = arr2.get(arr2.size() - 1);
+
+            if (last1 > last2) {
                 arr1.add(nums[i]);
-            } else{
+            } else {
                 arr2.add(nums[i]);
             }
-
         }
 
-        int result[] = new int[arr1.size() + arr2.size()];
-        int i =0;
-        for(int ele : arr1) {
-            result[i++] = ele;
+        // Concatenate arr1 + arr2
+        int[] result = new int[nums.length];
+        int index = 0;
+
+        for (int num : arr1) {
+            result[index++] = num;
         }
-        for(int ele : arr2) {
-            result[i++] = ele;
+
+        for (int num : arr2) {
+            result[index++] = num;
         }
+
         return result;
-    }   
+    }
 }
